623bb5c816915a3311f0ac5312088f6fe63daf20
Richard Venneman <richardvenneman@me.com>
2019-06-18 18:24:41 -0300

    Update namespaced class notation

--- lib/instagrammer/user.rb
+++ lib/instagrammer/user.rb
@@ -1,29 +1,27 @@
 # frozen_string_literal: true
 
-module Instagrammer
-  class User
-    include Capybara::DSL
+class Instagrammer::User
+  include Capybara::DSL
 
-    attr_reader :name, :username, :avatar, :bio, :url, :follower_count
+  attr_reader :name, :username, :avatar, :bio, :url, :follower_count
 
-    def initialize(username)
-      @username = username
+  def initialize(username)
+    @username = username
 
-      get_data
-    end
+    get_data
+  end
 
-    def get_data
-      visit "https://www.instagram.com/#{@username}/"
-      raise UserNotFound.new("User not found: #{@username}") if page.title.include?("Page Not Found")
+  def get_data
+    visit "https://www.instagram.com/#{@username}/"
+    raise Instagrammer::UserNotFound.new("User not found: #{@username}") if page.title.include?("Page Not Found")
 
-      node = page.first(:json_ld, visible: false)
-      data = JSON.parse(node.text(:all))
-      @name = data["name"]
-      @username = data["name"]
-      @avatar = data["image"]
-      @bio = data["description"]
-      @url = data["url"]
-      @follower_count = data["mainEntityofPage"]["interactionStatistic"]["userInteractionCount"].to_i
-    end
+    node = page.first(:json_ld, visible: false)
+    data = JSON.parse(node.text(:all))
+    @name = data["name"]
+    @username = data["name"]
+    @avatar = data["image"]
+    @bio = data["description"]
+    @url = data["url"]
+    @follower_count = data["mainEntityofPage"]["interactionStatistic"]["userInteractionCount"].to_i
   end
 end
