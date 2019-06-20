561c22a114be10b358890fa7a6cd9939df5161cc
Richard Venneman <richardvenneman@me.com>
2019-06-20 00:50:16 -0300

    Update Selenium chromeOptions

--- lib/instagrammer/config/capybara.rb
+++ lib/instagrammer/config/capybara.rb
@@ -2,7 +2,15 @@
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(no-sandbox headless disable-gpu window-size=1400,1400) }
+    chromeOptions: {
+      args: %w(
+        no-sandbox
+        headless
+        disable-dev-shm-usage
+        disable-gpu
+        window-size=1400,1400
+      )
+    }
   )
 
   Capybara::Selenium::Driver.new app,
